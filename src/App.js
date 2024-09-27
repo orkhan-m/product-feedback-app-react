6943bfafd76358249ffa271161e3566746ed9d5a
gylman <gylmanbm@gmail.com>
2024-09-28 07:56:45 +0900

    refactor: class -> className, move other components into separate files

--- src/App.js
+++ src/App.js
@@ -1,55 +1,15 @@
 import "./index.css";
-// import { useState } from "react";
-import bulbSvg from "./assets/bulb.svg";
-import dropdownIcon from "./assets/dropdown_icon.svg";
-import manWithMagnifier from "./assets/man_with_magnifier.svg";
 
-const features = ["All", "UI", "UX", "Enhancement", "Bug", "Feature"];
+import FeedbackBoard from "./components/FeedbackBoard";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <div>
-      <div class="page-top">
+      <div className="page-top">
         <FeedbackBoard />
         <Header />
       </div>
-    </div>
-  );
-}
-
-function FeedbackBoard() {
-  return (
-    <div class="side-boxes">
-      <div class="feedback-board">
-        <div class="feedback-board-circle-one"></div>
-        <div class="feedback-board-circle-two"></div>
-        <div class="board-text">
-          <p class="board-main-text">Frontend Mentor</p>
-          <p class="board-secondary-text">Feedback Board</p>
-        </div>
-      </div>
-      {/* NOTE Category Filter */}
-      <div class="category-filter">
-        {features.map((feature) => (
-          <div className="feedback-features-filter">{feature}</div>
-        ))}
-      </div>
-    </div>
-  );
-}
-
-function Header() {
-  return (
-    <div class="nav-bar">
-      <img class="bulb-icon" src={bulbSvg} alt="Bulb Icon" />
-      <p class="nav-bar-suggestions">0 Suggestions</p>
-      <p class="sort-by">
-        Sort by :{" "}
-        <span class="dropdown-items">
-          Most Upvotes <img src={dropdownIcon} alt="Dropdown Icon"></img>
-        </span>
-      </p>
-      <button class="btn-add-feedback">+ Add Feedback</button>
     </div>
   );
 }
