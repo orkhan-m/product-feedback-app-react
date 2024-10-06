2e1cdc1a19f1adf69cb971ffc397b3c68eecf665
orkhan-m <orkhanmirzayev96@gmail.com>
2024-10-06 16:58:03 +0200

    change class names to camel case

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -4,9 +4,9 @@ const features = ["All", "UI", "UX", "Enhancement", "Bug", "Feature"];
 
 export default function CategoryFilter() {
   return (
-    <div className={styles.category_filter}>
+    <div className={styles.categoryFilter}>
       {features.map((feature) => (
-        <button key={feature} className={styles.feedback_features_filter}>
+        <button key={feature} className={styles.feedbackFeaturesFilter}>
           {feature}
         </button>
       ))}
